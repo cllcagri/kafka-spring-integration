76638280941a20e28088bfc2e491cdd8c9eab765
Cagri Yilmaz <c.cagriyilmaz@gmail.com>
2022-02-06 16:50:49 +0300

    Test package change

--- src/test/java/com/springbootkafka/example/kafkaSpringConnection/ApacheKafkaProducerDemoApplicationTests.java
+++ src/test/java/com/springbootkafka/example/kafkaSpringConnection/ApacheKafkaProducerDemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.springbootkafka.example.apachekafkaproducerdemo;
+package com.springbootkafka.example.kafkaSpringConnection;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
